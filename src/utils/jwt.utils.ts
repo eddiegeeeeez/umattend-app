3093f963b6eab3fa05abb242b9a222786ebe2731
riomar0001 <inguitomario00@gmail.com>
2025-09-15 06:24:09 +0800

    chore: create utils

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,95 @@
+import jwt from 'jsonwebtoken';
+import prisma from '../configs/prisma.config';
+import { hashRefreshToken } from '../utils/tokenHashing';
+import { v4 as uuidv4 } from 'uuid';
+import { AccessTokenPayload } from '../types/token';
+import '@/configs/dotenv.config';
+import { sign } from 'jsonwebtoken';
+
+export const generateAccessToken = (
+  tokenPayload: AccessTokenPayload
+): string => {
+  const SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
+
+  if (!SECRET) {
+    throw new Error('JWT Access Token Secret is not defined.');
+  }
+
+  const {
+    user_id,
+    student_id,
+    umindanao_email,
+    first_name,
+    last_name,
+    department,
+    program,
+    role,
+  } = tokenPayload;
+
+  if (
+    !user_id ||
+    !student_id ||
+    !umindanao_email ||
+    !first_name ||
+    !last_name ||
+    !department ||
+    !program ||
+    !role
+  ) {
+    throw new Error('Missing required token payload fields');
+  }
+
+  return sign(
+    {
+      user_id,
+      student_id,
+      umindanao_email,
+      first_name,
+      last_name,
+      department,
+      program,
+      role,
+    },
+    SECRET,
+    {
+      expiresIn: '1h',
+    }
+  );
+};
+
+export const generateRefreshToken = async (
+  user_id: string,
+  ip: string,
+  user_agent: string
+) => {
+  const SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
+
+  if (!SECRET) {
+    throw new Error('JWT refresh secret not defined');
+  }
+
+  const token_id = uuidv4();
+
+  const expires_at = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const token = jwt.sign({ token_id, user_id }, SECRET, {
+    expiresIn: '7d',
+  });
+
+  const hashedToken = await hashRefreshToken(token);
+
+  await prisma.refresh_token.create({
+    data: {
+      id: token_id,
+      user_id,
+      token_hash: hashedToken,
+      ip_address: ip,
+      user_agent: user_agent || 'Unknown Device',
+      device: user_agent || 'Unknown Device',
+      expires_at,
+      last_used: new Date(),
+    },
+  });
+
+  return token;
+};
---
+++ src/utils/responseHandler.ts
@@ -0,0 +1,36 @@
+import { Response } from 'express';
+
+interface SuccessResponse<T = unknown> {
+  success: true;
+  message: string;
+  data: T;
+}
+
+interface ErrorResponse {
+  success: false;
+  message: string;
+}
+
+export const successResponse = (
+  res: Response,
+  statusCode: number,
+  message: string,
+  data: any = null
+): Response<SuccessResponse> => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+export const errorResponse = (
+  res: Response,
+  statusCode: number,
+  error: any
+): Response<ErrorResponse> => {
+  return res.status(statusCode).json({
+    success: false,
+    message: error,
+  });
+};
