4a95805c1f3fde529fe2091d8da10028d8c57004
riomar0001 <inguitomario00@gmail.com>
2025-10-16 15:46:43 +0800

    feat: Add profile picture handling in onboarding and login processes
    Adds user profile picture support to onboarding and login
    
    Enables handling and display of user profile pictures during onboarding and login, improving personalization and consistency across authentication flows.
    Updates types and state to include profile picture, ensuring data is propagated from backend to frontend.

--- server/src/v1/services/auth.service.ts
+++ server/src/v1/services/auth.service.ts
@@ -72,7 +72,11 @@ const googleAuthWithCode = async (
     name: user.student?.name,
     department: user.student?.department ?? '',
     program: user.student?.program ?? '',
+    profile_picture: user.student?.profile_picture ?? '',
   });
+
+  console.log(user);
+  
 
   const refresh_token = await generateRefreshToken(
     user.id,
