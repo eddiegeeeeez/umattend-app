78ffe84f71f8107c33ff2e97d08a88032782caf4
riomar0001 <inguitomario00@gmail.com>
2025-10-20 11:33:02 +0800

    fix: update SMTP configuration to use Gmail host directly

--- server/src/configs/smtp.config.ts
+++ server/src/configs/smtp.config.ts
@@ -2,15 +2,10 @@ import nodemailer from 'nodemailer';
 import {
   MAIL_USER,
   MAIL_PASS,
-  MAIL_HOST,
-  MAIL_PORT,
-  MAIL_SECURE,
 } from '@/constants/smtp.constants';
 
 export const transporter = nodemailer.createTransport({
-  host: MAIL_HOST,
-  port: Number(MAIL_PORT),
-  secure: MAIL_SECURE === 'true',
+  host: "Gmail",
   auth: {
     user: MAIL_USER,
     pass: MAIL_PASS,
