169f3084fadf9c789603f82feb2e4e01ad0f3f6b
riomar0001 <inguitomario00@gmail.com>
2025-10-20 11:59:40 +0800

    fix: rever back to speicif host and port

--- server/src/configs/smtp.config.ts
+++ server/src/configs/smtp.config.ts
@@ -2,10 +2,15 @@ import nodemailer from 'nodemailer';
 import {
   MAIL_USER,
   MAIL_PASS,
+  MAIL_HOST,
+  MAIL_PORT,
+  MAIL_SECURE,
 } from '@/constants/smtp.constants';
 
 export const transporter = nodemailer.createTransport({
-  host: "Gmail",
+  host: MAIL_HOST,
+  port: Number(MAIL_PORT),
+  secure: MAIL_SECURE === 'true',
   auth: {
     user: MAIL_USER,
     pass: MAIL_PASS,
